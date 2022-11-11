9766ade9ba5f6cbfbd627f6939daf5cd6fdaa46c
Otakar Kočí <koci.ota02@gmail.com>
2022-11-11 10:04:32 +0100

    update

--- src/restaurant_list.h
+++ src/restaurant_list.h
@@ -5,12 +5,27 @@
 // create list, add restaurant, remove, get list, get_ids?, get_restaurant?, delete
 
 typedef struct RESTAURANT{
-    unsigned int        id;
+    //unsigned int        id;
     char                name[30];
     char                address[40];
     unsigned short int  type;
     char                description[200];
 
 }RESTAURANT;
+
+typedef struct RESTAURANT_ITEM{
+    unsigned int    id;
+    RESTAURANT*     data;
+}RESTAURANT_ITEM;
+
+typedef struct RESTAURANT_LIST{
+    RESTAURANT_ITEM*    head;
+    RESTAURANT_ITEM*    current;
+    RESTAURANT_ITEM*    tail;
+}RESTAURANT_LIST;
+
+RESTAURANT_LIST* createRestaurantList();
+void deleteRestaurantList();
+
 
 #endif
